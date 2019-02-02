40ca4479bc16037f958c3822e5c849d2fb026c62
bluessista <inga@bluessista.com>
2019-02-02 15:43:26 +0100

    add ListContacts to state in App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React, { Component } from 'react';
+import ListContacts from "./ListContacts";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        Hello World
-      </div>
-    );
-  }
+    state = {
+        contacts: [
+            {
+                "id": "inga",
+                "name": "inga bluessista",
+                "handle": "ibluessista",
+                "avatarURL": "http://localhost:5001/karen.jpg"
+              },
+              {
+                "id": "sarah",
+                "name": "sarah drasner",
+                "handle": "sarahdrasner",
+                "avatarURL": "http://localhost:5001/richard.jpg"
+              },
+              {
+                "id": "tyler",
+                "name": "tyler mcginnis",
+                "handle": "tylermcginnis",
+                "avatarURL": "http://localhost:5001/tyler.jpg"
+              }
+        ]
+    }
+
+    removeContact = (contact) => {
+        this.setState((currentState) => ({
+            contacts: currentState.contacts.filter(c => 
+               c.id !== contact.id
+            )
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+            <ListContacts 
+                contacts={this.state.contacts}
+                onDeleteContact={this.removeContact}
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
